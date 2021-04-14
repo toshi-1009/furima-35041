5686ac79392d322e41e3b1f25c05817be45b3292
toshi-1009 <dearstage0429@gmail.com>
2021-04-14 16:53:05 +0900

    出品機能の実装③

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
 
   def index
     @item = Item.order('created_at DESC')
