9275e1e4aa3c0fd32e3bc6fc658fa495192d7936
toshi-1009 <dearstage0429@gmail.com>
2021-04-16 14:56:52 +0900

    商品情報編集の実装③

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update]
   before_action :set_item, only: [:show,:edit,:update]
+  before_action :contributor_confirmation, only: [:edit, :update]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -45,7 +45,6 @@ class ItemsController < ApplicationController
   end
 
   def contributor_confirmation
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 end
