157cd06c4231adfe0386e1012dd259134964dc6e
toshi-1009 <dearstage0429@gmail.com>
2021-04-21 10:24:44 +0900

    商品購入機能の実装②

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -13,11 +13,9 @@ const pay = () => {
       exp_month: formData.get("order_address[exp_month]"),
       exp_year: `20${formData.get("order_address[exp_year]")}`,
     };
-    console.log(card);
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
-        console.log(token)
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
